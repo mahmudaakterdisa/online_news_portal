28ead7095eb184eab0c866036e5049dd01da50b8
MahmudaDisa <dishamirza.h@gmail.com>
2022-09-03 00:33:55 +0200

    catagories section done

---
+++ js/news.js
@@ -0,0 +1,36 @@
+// steps:fetch the url
+const loadData = () => {
+    fetch(" https://openapi.programming-hero.com/api/news/categories")
+        .then(response => response.json())
+        .then(data => displayCatagories(data.data.news_category))
+}
+
+const displayCatagories = (data) => {
+
+    const categories = document.getElementById("catagories")
+    data.forEach(catagory => {
+        // console.log(catagory);
+
+        const div = document.createElement("div")
+        div.classList.add("catagories-color")
+        //catagories function also add here
+        div.innerHTML = `
+<h3 onclick="catagoriesDetails('${catagory.category_id}')">${catagory.category_name}</h3>
+
+`;
+        categories.appendChild(div);
+    });
+
+
+}
+
+const catagoriesDetails = id => {
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+loadData();
